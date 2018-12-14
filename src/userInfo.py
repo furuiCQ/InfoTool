2495612db8b8f4189a964701996ea040db009bd3
furui@farpower <1182590636@qq.com>
2018-12-14 16:52:17 +0800

    update

--- src/userInfo.py
+++ src/userInfo.py
@@ -31,11 +31,23 @@ class Info:
                     if len(text) > 0:  # 去掉空数据
                         if text != lastRow:
                             lastRow = text
-                            self.getUserName(text.replace(" ", "") + "\n")
-                            self.getUserNation(text.replace(" ", "") + "\n")
-                            temp += (text.replace(" ", "") + "\n")  # 添加换行，方便抓取
+                            content = text.replace(" ", "") + "\n"
+
+                            self.getUserName(content)
+                            self.getUserNation(content)
+                            self.getUserBirth(content)
+                            self.getUserHeight(content)
+                            self.getUserPhone(content)
+                            self.getUserPolity(content)
+                            self.getUserEmail(content)
+                            self.getUserSchool(content)
+                            self.getUserAddress(content)
+                            self.getUserEdu(content)
+
+                            temp += content  # 添加换行，方便抓取
 
     # print temp
+
     def getPropty(self, text, temp):
         text = text[text.find(temp):]
         return text[text.find(temp):text.find('\n')].replace(temp, "").replace(":", "").replace("：", "")
@@ -55,3 +67,27 @@ class Info:
     def getUserHeight(self, text):
         if text.find('身高') != -1:
             self.height = self.getPropty(text, '身高')
+
+    def getUserPhone(self, text):
+        if text.find('电话') != -1:
+            self.phone = self.getPropty(text, '电话')
+
+    def getUserPolity(self, text):
+        if text.find('政治面貌') != -1:
+            self.polity = self.getPropty(text, '政治面貌')
+
+    def getUserEmail(self, text):
+        if text.find('邮箱') != -1:
+            self.email = self.getPropty(text, '邮箱')
+
+    def getUserSchool(self, text):
+        if text.find('毕业院校') != -1:
+            self.school = self.getPropty(text, '毕业院校')
+
+    def getUserAddress(self, text):
+        if text.find('住址') != -1:
+            self.address = self.getPropty(text, '住址')
+
+    def getUserEdu(self, text):
+        if text.find('学历') != -1:
+            self.edu = self.getPropty(text, '学历')
